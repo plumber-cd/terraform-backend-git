e8efd892dc939d8dfcf6f89e85a2c9ccc43af0fa
Dee Kryvenko <109895+dee-kryvenko@users.noreply.github.com>
2020-04-12 21:31:45 -0700

    Implemented wrapper mode (#3) (#7)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.0.3] - 2020-04-12
+
+### Added
+
+- Implemented wrapper mode (#3)
+
 ## [0.0.2] - 2020-04-12
 
 ### Added
---
+++ tf_backend_wrapper.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+// terraformWrapperCmd will pass all arguments to terraform,
+// given that it was started from under the wrapper - will be pointing to this backend
+var terraformWrapperCmd = &cobra.Command{
+	Use:                   "terraform",
+	Short:                 "Run terraform while storage is running",
+	DisableFlagsInUseLine: true,
+	SilenceUsage:          true,
+	Args:                  cobra.ArbitraryArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		tf, err := cmd.Flags().GetString("tf")
+		if err != nil {
+			return err
+		}
+
+		tfCommand := exec.Command(tf, args...)
+		tfCommand.Stdin = os.Stdin
+		tfCommand.Stdout = os.Stdout
+		tfCommand.Stderr = os.Stderr
+
+		if err := tfCommand.Run(); err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
