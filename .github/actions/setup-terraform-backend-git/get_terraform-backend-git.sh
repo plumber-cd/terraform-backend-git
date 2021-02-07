8b73a014a3b92266e41677306eae865a19743046
dom <dominik.meyer01@sap.com>
2021-02-07 10:29:10 +0100

    Change x permission

--- .github/actions/setup-terraform-backend-git/get_terraform-backend-git.sh
+++ .github/actions/setup-terraform-backend-git/get_terraform-backend-git.sh
@@ -14,5 +14,6 @@ fi
 mkdir terraform-backend-git
 TARGET_FILE="terraform-backend-git"
 curl -LJ -o terraform-backend-git/$TARGET_FILE 'https://github.com/plumber-cd/terraform-backend-git/releases/download/'"$INPUT_VERSION"'/terraform-backend-git-darwin-386'
+chmod +x terraform-backend-git/$TARGET_FILE
 echo "terraform-backend-git" >> $GITHUB_PATH
 echo "::set-output name=version::$INPUT_VERSION"
