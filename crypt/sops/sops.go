fc6aa9c238994347ac9a3aaa0adc2189fac95f6e
Dee Kryvenko <llibicpep@gmail.com>
2022-05-15 01:10:06 -0700

    Add Hashicorp Vault to sops

--- crypt/sops/sops.go
+++ crypt/sops/sops.go
@@ -1,15 +1,10 @@
 package sops
 
 import (
-	"os"
+	"log"
 
 	sops "go.mozilla.org/sops/v3"
-	"go.mozilla.org/sops/v3/pgp"
 )
-
-func init() {
-	Configs["pgp"] = &PGPConfig{}
-}
 
 type Config interface {
 	IsActivated() bool
@@ -21,8 +16,9 @@ var Configs = make(map[string]Config)
 func GetActivatedKeyGroups() ([]sops.KeyGroup, error) {
 	keyGroups := make([]sops.KeyGroup, 0)
 
-	for _, config := range Configs {
+	for provider, config := range Configs {
 		if config.IsActivated() {
+			log.Printf("Activating %q encryption provider", provider)
 			kg, err := config.KeyGroup()
 			if err != nil {
 				return nil, err
@@ -32,23 +28,4 @@ func GetActivatedKeyGroups() ([]sops.KeyGroup, error) {
 	}
 
 	return keyGroups, nil
-}
-
-type PGPConfig struct{}
-
-func (c *PGPConfig) IsActivated() bool {
-	_, ok := os.LookupEnv("TF_BACKEND_HTTP_SOPS_PGP_FP")
-	return ok
-}
-
-func (c *PGPConfig) KeyGroup() (sops.KeyGroup, error) {
-	fp := os.Getenv("TF_BACKEND_HTTP_SOPS_PGP_FP")
-
-	var keyGroup sops.KeyGroup
-
-	for _, k := range pgp.MasterKeysFromFingerprintString(fp) {
-		keyGroup = append(keyGroup, k)
-	}
-
-	return keyGroup, nil
 }
