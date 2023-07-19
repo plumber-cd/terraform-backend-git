1fee45198d700d05d74da0dc4619948238ee0c91
Victor Zagorski <victor@zagorski.ru>
2023-07-19 12:04:40 +0700

    Added GCP KMS support for sops encryption

---
+++ crypt/sops/gcp_kms.go
@@ -0,0 +1,31 @@
+package sops
+
+import (
+	"os"
+
+	sops "go.mozilla.org/sops/v3"
+	"go.mozilla.org/sops/v3/gcpkms"
+)
+
+func init() {
+	Configs["gcp-kms"] = &GcpKmsConfig{}
+}
+
+type GcpKmsConfig struct{}
+
+func (c *GcpKmsConfig) IsActivated() bool {
+	_, ok := os.LookupEnv("TF_BACKEND_HTTP_SOPS_GCP_KMS_KEYS")
+	return ok
+}
+
+func (c *GcpKmsConfig) KeyGroup() (sops.KeyGroup, error) {
+	keys := os.Getenv("TF_BACKEND_HTTP_SOPS_GCP_KMS_KEYS")
+
+	var keyGroup sops.KeyGroup
+
+	for _, k := range gcpkms.MasterKeysFromResourceIDString(keys) {
+		keyGroup = append(keyGroup, k)
+	}
+
+	return keyGroup, nil
+}
